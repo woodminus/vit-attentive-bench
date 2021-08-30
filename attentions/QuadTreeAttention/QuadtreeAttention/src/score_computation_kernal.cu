065204b13a5f46d16096f2a12b4d83317ead07a9
woodminus <160528767+woodminus@users.noreply.github.com>
2021-08-30 16:01:37 +0000

    Add type definitions

--- attentions/QuadTreeAttention/QuadtreeAttention/src/score_computation_kernal.cu
+++ attentions/QuadTreeAttention/QuadtreeAttention/src/score_computation_kernal.cu
@@ -3,4 +3,14 @@
 #include <math.h>
 #include <float.h>
 #include <vector>
-#include "score_computat
+#include "score_computation.h"
+#include <stdio.h>
+
+#define ROUND_OFF 50000
+
+#define CUDA_NUM_THREADS 1024
+#define WARPS_PER_BLOCK 1
+#define THREADS_PER_WARP 32
+#define MAX_H 8
+
+#define CUDA_KERNEL_LOOP(i, n) fo
