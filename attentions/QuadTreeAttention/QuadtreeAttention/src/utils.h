24b5d36bb4f71b887f04b3e2303875305e2f3153
woodminus <160528767+woodminus@users.noreply.github.com>
2021-10-22 16:04:13 +0000

    Remove usage of TypeSwitch.

--- attentions/QuadTreeAttention/QuadtreeAttention/src/utils.h
+++ attentions/QuadTreeAttention/QuadtreeAttention/src/utils.h
@@ -5,4 +5,12 @@
 class Formatter {
 public:
   Formatter() {}
-  ~Formatter() {
+  ~Formatter() {}
+
+  template <typename Type> Formatter &operator<<(const Type &value) {
+    stream_ << value;
+    return *this;
+  }
+
+  std::string str() const { return stream_.str(); }
+  operator std::string() const { return 
