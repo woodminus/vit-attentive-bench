09ce755248c46a04d874905048bf08d061d0d1f5
woodminus <160528767+woodminus@users.noreply.github.com>
2021-10-25 19:06:50 +0000

    testing commit

--- attentions/QuadTreeAttention/QuadtreeAttention/src/utils.h
+++ attentions/QuadTreeAttention/QuadtreeAttention/src/utils.h
@@ -13,4 +13,12 @@ public:
   }
 
   std::string str() const { return stream_.str(); }
-  operator std::string() const { return 
+  operator std::string() const { return stream_.str(); }
+
+  enum ConvertToString { to_str };
+
+  std::string operator>>(ConvertToString) { return stream_.str(); }
+
+private:
+  std::stringstream stream_;
+  Formatter(co
