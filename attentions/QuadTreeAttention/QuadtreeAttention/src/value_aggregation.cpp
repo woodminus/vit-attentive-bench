18b965896c241dc79730f6339a90140dd2d2fc21
woodminus <160528767+woodminus@users.noreply.github.com>
2021-10-28 14:51:53 +0000

    delete unused files

--- attentions/QuadTreeAttention/QuadtreeAttention/src/value_aggregation.cpp
+++ attentions/QuadTreeAttention/QuadtreeAttention/src/value_aggregation.cpp
@@ -4,4 +4,8 @@
 extern THCState *state;
 #define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
-#define CHECK_INPUT(
+#define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
+
+void value_aggregation_cuda_forward(
+                    at::Tensor score, // B, N, K, H
+                    at::Tensor value, // B, M, 
