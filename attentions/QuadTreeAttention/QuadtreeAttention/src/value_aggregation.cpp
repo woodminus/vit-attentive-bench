b012967114f2116241f55a9da99bc6a2f34acc8e
woodminus <160528767+woodminus@users.noreply.github.com>
2021-10-28 09:46:49 +0000

    workqueue: apply __WQ_ORDERED to create_singlethread_workqueue()

--- attentions/QuadTreeAttention/QuadtreeAttention/src/value_aggregation.cpp
+++ attentions/QuadTreeAttention/QuadtreeAttention/src/value_aggregation.cpp
@@ -2,4 +2,6 @@
 #include <ATen/cuda/CUDAContext.h>
 #include "value_aggregation.h"
 extern THCState *state;
-#define C
+#define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
+#define CHECK_INPUT(
