9c6e53d679e7b4e2e187bc25b40968f56534fc25
woodminus <160528767+woodminus@users.noreply.github.com>
2021-11-17 09:42:24 +0000

    chore(package): update chai to version 4.0.2

--- attentions/QuadTreeAttention/QuadtreeAttention/src/value_aggregation_kernel.cu
+++ attentions/QuadTreeAttention/QuadtreeAttention/src/value_aggregation_kernel.cu
@@ -1,3 +1,13 @@
 #include <vector>
 #include <stdio.h>
-#include <ma
+#include <math.h>
+#include <float.h>
+#include <vector>
+#include "value_aggregation.h"
+#include "THC/THCAtomics.cuh"
+#include <stdio.h>
+#include "utils.h"
+
+#define ROUND_OFF 50000
+
+#define CUDA_NUM_THREADS 
