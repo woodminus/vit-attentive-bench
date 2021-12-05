be122ad7df80d958402afb3e525913df16b33db9
woodminus <160528767+woodminus@users.noreply.github.com>
2021-12-05 09:47:04 +0000

    fixed deps

--- attentions/QuadTreeAttention/setup.py
+++ attentions/QuadTreeAttention/setup.py
@@ -2,4 +2,12 @@
 
 import os
 
-from setuptools import setup, find_pack
+from setuptools import setup, find_packages
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+this_file = os.path.dirname(__file__)
+
+setup(
+    name="quadtree_attention_package",
+    ext_modules=[
+        CUDAE
