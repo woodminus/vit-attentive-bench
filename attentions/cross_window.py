85c7b14da0d41ccd4d1fa78324764b99195a78d5
woodminus <160528767+woodminus@users.noreply.github.com>
2021-12-26 18:21:54 +0000

    Add version badge to README

--- attentions/cross_window.py
+++ attentions/cross_window.py
@@ -1,3 +1,15 @@
 """
     Paper: CSWin Transformer: A General Vision Transformer Backbone with Cross-Shaped
-    Link: https://arxi
+    Link: https://arxiv.org/abs/2107.00652
+"""
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+def img2windows(img, H_sp, W_sp):
+    """
+    img: B C H W
+    """
+    B, C, H, W = img.shape
+  
