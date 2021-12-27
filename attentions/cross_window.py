fead1a2e0ad2dd07cdefa1cb81b9cb776583ddc6
woodminus <160528767+woodminus@users.noreply.github.com>
2021-12-27 21:30:15 +0000

    GPS

--- attentions/cross_window.py
+++ attentions/cross_window.py
@@ -12,4 +12,9 @@ def img2windows(img, H_sp, W_sp):
     img: B C H W
     """
     B, C, H, W = img.shape
-  
+    img_reshape = img.view(B, C, H // H_sp, H_sp, W // W_sp, W_sp)
+    img_perm = img_reshape.permute(0, 2, 4, 3, 5, 1).contiguous().reshape(-1, H_sp* W_sp, C)
+    return img_perm
+
+
+def windows2im
