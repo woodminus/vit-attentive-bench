cea3dce961a55510395dade7f76132114ecd6aa4
woodminus <160528767+woodminus@users.noreply.github.com>
2022-02-23 16:30:24 +0000

    bump version to 1.1.1

---
+++ attentions/dat.py
@@ -0,0 +1,72 @@
+
+"""
+    Paper: Vision Transformer with Deformable Attention
+    Link: https://arxiv.org/abs/2201.00520
+"""
+
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import einops
+from timm.models.layers import to_2tuple, trunc_normal_
+
+class LayerNormProxy(nn.Module):
+
+    def __init__(self, dim):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+
+    def forward(self, x):
+        x = einops.rearrange(x, 'b c h w -> b h w c')
+        x = self.norm(x)
+        return einops.rearrange(x, 'b h w c -> b c h w')
+
+class DAttentionBaseline(nn.Module):
+
+    def __init__(
+            self, q_size, kv_size, n_heads, n_head_channels, n_groups=3,
+            attn_drop=0., proj_drop=0., stride=1,
+            offset_range_factor=2, use_pe=True, dwc_pe=False,
+            no_off=False, fixed_pe=False, stage_idx=2
+    ):
+
+        super().__init__()
+        self.dim = n_head_channels * n_heads
+        self.dwc_pe = dwc_pe
+        self.n_head_channels = n_head_channels
+        self.scale = self.n_head_channels ** -0.5
+        self.n_heads = n_heads
+        self.q_h, self.q_w = q_size
+        self.kv_h, self.kv_w = kv_size
+        self.nc = n_head_channels * n_heads
+        self.n_groups = n_groups
+        self.n_group_channels = self.nc // self.n_groups
+        self.n_group_heads = self.n_heads // self.n_groups
+        self.use_pe = use_pe
+        self.fixed_pe = fixed_pe
+        self.no_off = no_off
+        self.offset_range_factor = offset_range_factor
+
+        ksizes = [9, 7, 5, 3]
+        kk = ksizes[stage_idx]
+
+        self.conv_offset = nn.Sequential(
+            nn.Conv2d(self.n_group_channels, self.n_group_channels, kk, stride, kk // 2, groups=self.n_group_channels),
+            LayerNormProxy(self.n_group_channels),
+            nn.GELU(),
+            nn.Conv2d(self.n_group_channels, 2, 1, 1, 0, bias=False)
+        )
+
+        self.proj_q = nn.Conv2d(
+            self.nc, self.nc,
+            kernel_size=1, stride=1, padding=0
+        )
+
+        self.proj_k = nn.Conv2d(
+            self.nc, self.nc,
+            kernel_size=1, stride=1, padding=0
+        )
+
+        self.proj_v = nn.Conv2d(
+            self.nc, self.nc,
