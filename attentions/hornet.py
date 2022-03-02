090ce1f224593bb67e7929bfa2d0dfd38d945ba0
woodminus <160528767+woodminus@users.noreply.github.com>
2022-03-02 13:59:34 +0000

    Quick fix.

--- attentions/hornet.py
+++ attentions/hornet.py
@@ -3,4 +3,11 @@
     Link: https://arxiv.org/abs/2207.14284
 """
 
-import 
+import torch
+import torch.nn as nn
+
+def get_dwconv(dim, kernel, bias):
+    return nn.Conv2d(dim, dim, kernel_size=kernel, padding=(kernel-1)//2 ,bias=bias, groups=dim)
+
+class gnconv(nn.Module):
+    def 
