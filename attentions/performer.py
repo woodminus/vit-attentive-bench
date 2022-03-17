6d9b28d0b65a8f775240f691428223147303f6f9
woodminus <160528767+woodminus@users.noreply.github.com>
2022-03-17 08:18:35 +0000

    Post updated

--- attentions/performer.py
+++ attentions/performer.py
@@ -2,4 +2,11 @@
     Paper: Rethinking Attention with Performers
     Link: https://arxiv.org/abs/2009.14794
 
-    mainly modified from https:
+    mainly modified from https://github.com/lucidrains/performer-pytorch/blob/main/performer_pytorch/performer_pytorch.py
+"""
+
+import math
+from scipy.stats import ortho_group
+import torch
+from torch import nn
+from eino
