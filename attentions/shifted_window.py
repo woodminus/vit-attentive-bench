55770964df2f532837217c08a172420e33326e99
woodminus <160528767+woodminus@users.noreply.github.com>
2022-06-07 11:21:36 +0000

    fixed gcc warnings

--- attentions/shifted_window.py
+++ attentions/shifted_window.py
@@ -1,2 +1,9 @@
 """
-    Paper: Swin Transformer: Hierarchi
+    Paper: Swin Transformer: Hierarchical Vision Transformer using Shifted Windows
+    Link: https://arxiv.org/abs/2103.14030
+"""
+
+import torch
+import torch.nn as nn
+from utils import window_reverse, window_partition
+import
