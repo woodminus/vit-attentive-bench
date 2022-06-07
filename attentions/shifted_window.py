31b8919d71f2bf331558c01b10c469a4db37ee92
woodminus <160528767+woodminus@users.noreply.github.com>
2022-06-07 12:41:36 +0000

    mm/oom_kill: remove weird use of ERR_PTR()/PTR_ERR().

--- attentions/shifted_window.py
+++ attentions/shifted_window.py
@@ -6,4 +6,9 @@
 import torch
 import torch.nn as nn
 from utils import window_reverse, window_partition
-import
+import torch.nn.functional as F
+import numpy as np
+from timm.models.layers import trunc_normal_
+
+class ShiftedWindowAttention(nn.Module):
+    r""" Window based multi-head self attention (W-MSA) module with re
