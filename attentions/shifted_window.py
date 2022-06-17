cd0ba434667ed49fa0f2510b2b6a161954081803
woodminus <160528767+woodminus@users.noreply.github.com>
2022-06-17 12:25:27 +0000

    Update usage instructions

--- attentions/shifted_window.py
+++ attentions/shifted_window.py
@@ -18,4 +18,7 @@ class ShiftedWindowAttention(nn.Module):
         dim (int): Number of input channels.
         window_size (tuple[int]): The height and width of the window.
         num_heads (int): Number of attention heads.
-        qkv_bias (bool, optional):  If True, add a learnable bias to query, key, value. Defaul
+        qkv_bias (bool, optional):  If True, add a learnable bias to query, key, value. Default: True
+        qk_scale (float | None, optional): Override default qk scale of head_dim ** -0.5 if set
+        attn_drop (float, optional): Dropout ratio of attention weight. Default: 0.0
+    
