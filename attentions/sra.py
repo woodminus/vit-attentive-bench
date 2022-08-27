0d3e4a506d599e178a6ea2da895ac42962477181
woodminus <160528767+woodminus@users.noreply.github.com>
2022-08-27 08:23:24 +0000

    Removed unnecessary includes

--- attentions/sra.py
+++ attentions/sra.py
@@ -1,2 +1,14 @@
 """
     Paper: Pyramid Vision Transformer: A Versatile Backbone for Dense Prediction without Convolutions
+    Link: https://arxiv.org/abs/2102.12122
+"""
+
+import torch
+import torch.nn as nn
+from utils import conv_flops
+
+class SRAttention(nn.Module):
+    """
+    Spatial Reduction Attention
+
+    Paper: Pyramid Vision Transforme
