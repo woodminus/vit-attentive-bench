8336b758da757a5aeaeb9bc2bc8a8ed57d499f72
woodminus <160528767+woodminus@users.noreply.github.com>
2022-10-12 18:07:07 +0000

    update jquery

--- attentions/utils.py
+++ attentions/utils.py
@@ -8,4 +8,10 @@ def measure_flops_params(model, x):
     n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
 
     flops = FlopCountAnalysis(model,  x)
-    converted 
+    converted = flops.total() / 1e6
+    print(f'Number of Params: {round(n_parameters/ 1e6, 2)} M')
+    print(f'FLOPs = {round(converted, 2)} M')
+
+
+def measure_throughput_gpu(model):
+    H = W = 1
