8b494c7345da7fabf6f8608731fbda9ea044cacf
woodminus <160528767+woodminus@users.noreply.github.com>
2022-10-12 17:23:10 +0000

    fixed composer

--- attentions/utils.py
+++ attentions/utils.py
@@ -1,3 +1,11 @@
 import torch
 import time
-from fvcore.nn import 
+from fvcore.nn import FlopCountAnalysis
+
+
+
+def measure_flops_params(model, x):
+    n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+    flops = FlopCountAnalysis(model,  x)
+    converted 
