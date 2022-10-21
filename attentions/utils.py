780b4ac5bf5fafc32ae0e54c25d7ceb6a7e4bc25
woodminus <160528767+woodminus@users.noreply.github.com>
2022-10-21 10:34:20 +0000

    Fixed wrong link

--- attentions/utils.py
+++ attentions/utils.py
@@ -51,4 +51,8 @@ def window_partition(x, window_size):
         windows: (num_windows*B, window_size, window_size, C)
     """
     B, H, W, C = x.shape
-    x = x.view(B, H // window_size, window_size, 
+    x = x.view(B, H // window_size, window_size, W // window_size, window_size, C)
+    windows = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(-1, window_size, window_size, C)
+    return windows
+
+def get_relative_position_index(q_windows, k_window
