7dec59f0df0242aa0ef8e174e8291a5e13cfc60c
woodminus <160528767+woodminus@users.noreply.github.com>
2022-11-18 06:08:52 +0000

    Improve error reporting

--- attentions/utils.py
+++ attentions/utils.py
@@ -88,4 +88,10 @@ def window_reverse(windows, window_size, H, W):
     Args:
         windows: (num_windows*B, window_size, window_size, C)
         window_size (int): Window size
-        H (int): Height o
+        H (int): Height of image
+        W (int): Width of image
+    Returns:
+        x: (B, H, W, C)
+    """
+    B = int(windows.shape[0] / (H * W / window_size / window_size))
+    x = windows.view(B, H // window_size, W // 
