38f48e6fce17178c0e6da4b43e63fd79d287fe99
woodminus <160528767+woodminus@users.noreply.github.com>
2022-12-02 18:06:59 +0000

    fsdf

--- plot.py
+++ plot.py
@@ -1,3 +1,11 @@
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+
+def plot_cpu():
+    data = {}
+    with open('results/attention_benchmark.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+        
