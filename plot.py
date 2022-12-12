e7f346f84b796111c35f4fbb64ff49df3030cf55
woodminus <160528767+woodminus@users.noreply.github.com>
2022-12-12 10:00:17 +0000

    deleted temp file

--- plot.py
+++ plot.py
@@ -8,4 +8,9 @@ def plot_cpu():
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
-        
+            if line_count == 0:
+                print(f'Column names are {", ".join(row)}')
+                line_count += 1
+            else:
+                data[row[0]] = {
+                    'params': row
